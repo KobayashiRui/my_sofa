b3a42d9b02ba7aae0aeff2bfd508513715fdeb99
Kobayashi Rui <roboroborx782@gmail.com>
2021-08-16 07:54:52 +0900

    update

--- STLtoVTKvolume/stl2volume.py
+++ STLtoVTKvolume/stl2volume.py
@@ -1,11 +1,10 @@
-import pymesh
+import Sofa.Core
 
-
-input_mesh = pymesh.load_mesh("./chamber_test1.stl")
-tetgen = pymesh.tetgen()
-tetgen.points = input_mesh.vertices # Input points.
-tetgen.triangles = input_mesh.faces # Input triangles
-tetgen.max_tet_volume = 0.01
-tetgen.verbosity = 0
-tetgen.run() # Execute tetgen
-mesh = tetgen.mesh # Extract output tetrahedral mesh.
+def createScene(rootNode):
+        rootNode.createObject('RequiredPlugin', pluginName='CGALPlugin')
+        node = rootNode.createChild('node')
+        node.createObject('Mesh',name='mesh',filename='./chamber_test1.stl')
+        node.createObject('MeshGenerationFromPolyhedron',inputPoints='@mesh.position', 
+                            inputTriangles='@mesh.triangles', drawTetras='1')
+        node.createObject('Mesh', position='@gen.outputPoints', tetrahedra='@gen.outputTetras')
+        node.createObject('VTKExporter', filename='chamber_test1', edges='0', tetras='1', exportAtBegin='1')
